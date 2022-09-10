41390c2c3174f7845e728b2c1e52158a27aa9035
tsuzuki557 <goodin557@gmail.com>
2022-09-10 14:02:51 +0900

    ツール追加

--- threejs/basic_minimum/src/main.ts
+++ threejs/basic_minimum/src/main.ts
@@ -1,7 +1,6 @@
 import './style.css';
 
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // 描画先のDOM指定
 const root = document.querySelector<HTMLElement>('#canvas-container');
@@ -18,11 +17,8 @@ const material = new THREE.MeshNormalMaterial();
 // const material = new THREE.MeshStandardMaterial();
 const mesh = new THREE.Mesh( geometry, material );
 
-// 座表軸
-const axes = new THREE.AxesHelper();
-
 // シーツにオブジェクトを追加
-scene.add(mesh, axes);
+scene.add(mesh);
 
 // カメラの初期化
 const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 1000);
@@ -31,17 +27,9 @@ camera.lookAt(scene.position);
 
 // レンダラーの初期化
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setClearColor(0xffffff, 1.0); // 背景色
+//renderer.setClearColor(0xffffff, 1.0); // 背景色
 //renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(width, height);
-
-// カメラコントローラー設定
-// const orbitControls = new OrbitControls(camera, renderer.domElement);
-// orbitControls.maxPolarAngle = Math.PI * 0.5;
-// orbitControls.minDistance = 0.1;
-// orbitControls.maxDistance = 100;
-//orbitControls.autoRotate = true;  // カメラの自動回転設定
-//orbitControls.autoRotateSpeed = 1.0;  // カメラの自動回転速度
 
 // レンダラーをDOMに追加
 root!.appendChild(renderer.domElement);
@@ -52,9 +40,6 @@ root!.appendChild(renderer.domElement);
 renderer.setAnimationLoop((time: DOMHighResTimeStamp) => {
   // mesh.rotation.x = time / 2000;
   // mesh.rotation.y = time / 1000;
-
-  // カメラコントローラーを更新
-  //orbitControls.update();
 
   // 描画する
   renderer.render(scene, camera);
